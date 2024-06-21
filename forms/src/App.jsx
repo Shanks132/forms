4405599cf313322732ebcbe3d89c28b7867b349a
Shanks <shankarkannan13@gmail.com>
2024-06-21 15:55:11 +0530

    Form level 1 completed

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -2,73 +2,63 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const[guestIn, setGuestIn]= useState(false)
-  const[submitted, setSubmitted]= useState(false)
-  
-  
+  const [guestIn, setGuestIn] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
+  const [userData, setUserData] = useState({})
 
-   function handleGuestStatus(){
+  function handleGuestStatus() {
     setGuestIn(!guestIn)
-   }
-  let userData={} ;
+  }
+
   function handleSubmit(event) {
     event.preventDefault()
     setSubmitted(true)
     const formData = new FormData(event.target)
-    const guestStatus = formData.get('guest')
     const data = Object.fromEntries(formData.entries())
 
-    if(!guestStatus){
-      data.guest = false;
-    }
-    else{
-      setGuestIn(true)
-      data.guest = true;
-    }
+    // Correct guest status handling
+    data.guest = formData.get('guest') ? true : false
     
+    setUserData(data)
     console.log(data)
-    }
+  }
 
   return (
     <div>
       <header id="title">
         Forms (level 1)
       </header>
-    { !submitted &&
+      {!submitted &&
+        <form id="form1" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input id='name' name='name' type="text" required></input>
 
-      <form id="form1" onSubmit={handleSubmit}>
-      <div>
-      <label htmlFor='name'>Name</label>
-      <input id='name' name='name' type="text" required></input>
+            <label htmlFor='email'>Email</label>
+            <input id='email' name='email' type="email" required></input>
 
-      <label htmlFor='email'>Email</label>
-      <input id='email' name='email' type="email" required></input>
+            <label htmlFor='age'>Age</label>
+            <input id='age' name='age' type="number" min={0}></input>
 
-      <label htmlFor='age'>Age</label>
-      <input id='age' name='age' type="number" min={0}></input>
+            <label htmlFor='guest'>Are you attending with a guest</label>
+            <input id='guest' name='guest' type="checkbox" onClick={handleGuestStatus}></input>
 
-      <label htmlFor='guest'>Are you attending with a guest</label>
-      <input id='guest' name='guest' type="checkbox" onClick={handleGuestStatus}></input>
-      
-      {guestIn && <>
-      <label htmlFor='guestName'>Guest Name</label>
-      <input id='guestName' name='guestName' type="text" required></input>
-      </>
+            {guestIn && <>
+              <label htmlFor='guestName'>Guest Name</label>
+              <input id='guestName' name='guestName' type="text" required></input>
+            </>}
+          </div>
+          <button id="submitButton" type="submit">Submit</button>
+        </form>
       }
-
-      </div>
-      <button id="submitButton" type="submit">Submit</button>
-    </form>
-    }
-    {submitted && 
-    <ul>
-      {Object.entries(userData).map(([key, value]) => 
-  <li key={key}>
-    <strong>{key}:</strong> {value}
-  </li>
-      )}
-    </ul>}
+      {submitted &&
+        <ul>
+          {Object.entries(userData).map(([key, value]) =>
+            <li key={key}>
+              <strong>{key}:</strong> {value.toString()}
+            </li>
+          )}
+        </ul>}
     </div>
   )
 }
